aa8b22eb8f8a571ae44bf9216d7f2c9ee5424ca3
Théo Ischia <theoischia@gmail.com>
2025-10-09 11:15:35 +0200

    fix(3-broadcast): fixing a typo

--- TP1/3-broadcast/broadcast.c
+++ TP1/3-broadcast/broadcast.c
@@ -12,11 +12,13 @@
 #define LOG_LEVEL LOG_LEVEL_INFO
 
 // TODO1 création structure message
-struct message {
+struct message
+{
     uint8_t seq_num;
     char text[BUFSIZE];
-} __attribute__((packed));;
- 
+} __attribute__((packed));
+;
+
 struct message m;
 
 /*---------------------------------------------------------------------------*/
@@ -36,7 +38,6 @@ void input_callback(const void *data, uint16_t len, const linkaddr_t *src,
     LOG_INFO_(" - Text: %s, Sequence: %d\n", msg->text, msg->seq_num);
 }
 
-
 PROCESS_THREAD(broadcast_button_process, ev, data)
 {
     PROCESS_BEGIN();
@@ -49,25 +50,26 @@ PROCESS_THREAD(broadcast_button_process, ev, data)
     // TODO2 créer le message à envoyer
     strcpy(m.text, "TEST");
 
-    while (1) {
+    while (1)
+    {
         // TODO3 attendre un événement de type "relâchement de bouton"
         PROCESS_WAIT_EVENT_UNTIL(button_hal_release_event);
-
 
         // TODO4 vérifier que l'événement est déclenché par le bouton 1 (par
         // ex.)
         button_hal_button_t *button = (button_hal_button_t *)data;
         uint8_t id = button->unique_id;
-        if(id == 0){
-      
+        if (id == 0)
+        {
+
             // TODO5 positionnez les variables associées à la couche nullnet
-            nullnet_buf = (uint8_t*)&m;
-            nullnet_len = strlen(m.text)+2;
+            nullnet_buf = (uint8_t *)&m;
+            nullnet_len = strlen(m.text) + 2;
 
             // envoi du message
             NETSTACK_NETWORK.output(NULL);
             LOG_INFO_("Message sent: %s (%d)\n", m.text, m.seq_num);
-            msg.seq_num++;
+            m.seq_num++;
         }
     }
 
